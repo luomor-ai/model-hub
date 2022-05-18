d358d3445edec1e8870819ff8a255b39e024372e
zhangchunsheng <zhangchunsheng423@gmail.com>
2022-05-18 11:36:11 +0800

    1. add human pose model

--- helmet-detection/client.py
+++ helmet-detection/client.py
@@ -12,12 +12,12 @@ def cv2_to_base64(image):
 
 if __name__ == '__main__':
     # 获取图片的base64编码格式
-    img1 = cv2_to_base64(cv2.imread("/home/aistudio/work/97ce96584315ba13a4927c8a0.jpg"))
+    img1 = cv2_to_base64(cv2.imread("HelmetDetection/images/hard_hat_workers1770.png"))
     data = {'images': [img1]}
     # 指定content-type
     headers = {"Content-type": "application/json"}
     # 发送HTTP请求
-    url = "http://127.0.0.1:8866/predict/helmet_hub"
+    url = "http://49.232.6.131:8866/predict/helmet-detection"
     r = requests.post(url=url, headers=headers, data=json.dumps(data))
 
     # 打印预测结果
