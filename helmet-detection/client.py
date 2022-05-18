0c2258fe3ce402099280fb1a515293fa2a37d7fc
zhangchunsheng <zhangchunsheng423@gmail.com>
2022-05-18 11:39:37 +0800

    1. add human pose model

--- helmet-detection/client.py
+++ helmet-detection/client.py
@@ -22,3 +22,17 @@ if __name__ == '__main__':
 
     # 打印预测结果
     print(r.json()["results"])
+
+    img = cv2.imread("HelmetDetection/images/hard_hat_workers1770.png")
+    for j in range(len(r.json()["results"][0])):
+        if (r.json()["results"][0][j]["score"] < 0.09):
+            continue
+        x, y, w, h = r.json()["results"][0][j]["bbox"]
+        x, y, w, h = int(x), int(y), int(w), int(h)
+        x2, y2 = x + w, y + h
+        # object_name = annos[j][""]
+        img = cv2.rectangle(img, (x, y), (x2, y2), (255, 0, 0), thickness=2)
+        img = cv2.putText(img, r.json()["results"][0][j]["category"], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75,
+                          (0, 0, 255), 2)
+        img_name = "HelmetDetection/result.png"
+        cv2.imwrite(img_name, img)
